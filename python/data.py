c7f93ff44f1c32170d056ca295ebf802bc23fb8c
avinasharavind <aviaravind15@gmail.com>
2024-07-22 19:01:34 -0400

    modified:   files/forecast.json 	modified:   files/hourData.csv 	modified:   files/hourly.json 	modified:   python/data.py

--- python/data.py
+++ python/data.py
@@ -8,6 +8,7 @@ def hourly_data(loc):
     lat, long = files.get_coords(loc)
     files.get_forecast(lat, long)
     hourly.get_hours()
+    print(time.time())
 
 def daily_data(loc):
     lat, long = files.get_coords(loc)
@@ -17,6 +18,7 @@ def daily_data(loc):
 
 schedule.every(30).minutes.at(":"+str(random.randint(10,45))).do(hourly_data, loc="Edison, NJ")
 schedule.every(30).minutes.at(":"+str(random.randint(10,45))).do(daily_data, loc="Edison, NJ")
+
 
 while True:
     schedule.run_pending()
