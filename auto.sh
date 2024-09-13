1ac4f92504943f77e6c1b774ddc4ffb02d6754ac
avinasharavind <aviaravind15@gmail.com>
2024-09-12 23:28:55 -0400

    modified:   auto.sh

--- auto.sh
+++ auto.sh
@@ -3,3 +3,4 @@ MESSAGE="Auto-commit: $(date)"
 REPO_PATH="/Users/avinasharavind/Documents/Weather_Projects/NWS_Forecast_Page"
 git -C "$REPO_PATH" add .
 git -C "$REPO_PATH" commit -m "$MESSAGE"
+git -C "$REPO_PATH" git push origin main
