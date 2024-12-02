549164ed036213633c04a78eba857c942c2a7d9c
avinasharavind <aviaravind15@gmail.com>
2024-12-02 17:03:28 -0500

    modified:   auto.sh 	modified:   index.html 	modified:   static/files/dayData.csv 	modified:   static/files/forecast.json 	modified:   static/files/griddata.json 	modified:   static/files/hourData.csv 	modified:   static/files/hourly.json

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-MESSAGE="Forecast Update (Automatic): $(date)"
-REPO_PATH="/Users/avinasharavind/Documents/Weather_Projects/NWS_Forecast_Page"
+#MESSAGE="Forecast Update (Automatic): $(date)"
+#REPO_PATH="/Users/avinasharavind/Documents/Weather_Projects/NWS_Forecast_Page"
 
-git -C "$REPO_PATH" add .
-git -C "$REPO_PATH" commit -m "$MESSAGE"
+#git -C "$REPO_PATH" add .
+#git -C "$REPO_PATH" commit -m "$MESSAGE"
